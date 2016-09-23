9447e269bfabe7ba2ce53db05a280ec4c8b3fcc9
Sunny Dai <Michael_Smeaton@hotmail.com>
2016-09-22 21:05:08 -0400

    Add files via upload
    Long method
    Step 1 - Create new method

--- src/model.py
+++ src/model.py
@@ -1,7 +1,5 @@
 import requests
-import re
 from bs4 import BeautifulSoup
-from decimal import Decimal
 from src.rule_type import *
 
 
@@ -64,76 +62,24 @@ class WebScraper:
         Method WebScraper.fetch()'s docstring.
         Get data from Web page.
         """
-        # html = ""
-        # try:
-        # file_handler = open(path, 'r+')
-        # html = file_handler.read()
-        # except FileNotFoundError:
-        # req = requests.get(url)
-        # html = req.content
         html = self.get_html_content(url)
-        # finally:
-        # soup = BeautifulSoup(html, 'html.parser')
-        # results = []
-        # results = self.fetch(html, maximum)
         results = self.fetch_data(html, max1, max2, *tag)
-        # content = soup.find_all('div', {'id': 'main'})
-        # for div in content:
-        # li = div.find_all('li', limit=maximum)
-        # li = self.fetch_data(html, maximum)
-        # for data in li:
-        # results.append(str(data))
         return results
 
     def fetch_from_file(self, path="", maximum=0):
         file_handler = open(path, 'r+')
         html = file_handler.read()
-        # results = self.fetch_from_url(html, maximum)
         results = self.fetch_data(html, maximum)
         return results
 
-        # def fetch_by_keyword(self, url, tag,
-        # tag_class, css_class, maximum=10):
-        # """
-        # Method WebScraper.fetch()'s docstring.
-        # Get data by keyword lookup from Web page.
-        # """
-        # req = requests.get(url)
-        # html = req.content
-        # html = self.get_html_content(url)
-        # soup = BeautifulSoup(html, 'html.parser')
-        # results = []
-        # content = soup.find_all('div', {'id': 'main'})
-        # for div in content:
-        # ul = div.find_all('li')
-        # for li in ul:
-        # ul = self.fetch_data(html, 0)
-        # results = self.fetch(html, 0,
-        # maximum, tag, {tag_class: css_class})
-        # for li in ul:
-        # span = li.find_all('span', {attr: keyword}, limit=maximum)
-        # for kw in span:
-        # results.append(str(kw))
-        # return results
-
     def fetch_data(self, html, max1=0, max2=0, *tag):
-        #  li = []
         results = []
         soup = BeautifulSoup(html, 'html.parser')
         content = soup.find_all('div', {'id': 'main'})
         for found in content:
             found = found.find_all("li", limit=max1)
             for data in found:
-                # if tag:
-                # li = div.find_all('li', limit=maximum)
-                # li = div.find_all(*tag, limit=max2)
                 good_data = data.find_all(*tag, limit=max2)
-                # for kw in li:
-                # for element in good_data:
-                # results.append(str(element))
-                # results.append(str(div))
-                # else:
-                # results.append(str(div))
                 [results.append(str(element)) for element in good_data] \
                     if tag else results.append(str(data))
         return results
@@ -160,3 +106,6 @@ class WebScraper:
             except AttributeError:
                 continue
         return results
+
+    def get_rule(self, rule_code):
+        pass
